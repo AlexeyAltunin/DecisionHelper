d8af427cceced1556a9ceb03b2aeb039c2d60953
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2018-01-18 21:29:42 +0100

    updated view

---
+++ DecisionHelper/OptionsCriteriaTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  OptionsCriteriaTableViewController.swift
+//  DecisionHelper
+//
+//  Created by ALEXEY ALTUNIN on 16/01/2018.
+//  Copyright © 2018 ALEXEY ALTUNIN. All rights reserved.
+//
+
+import UIKit
+
+class OptionsCriteriaTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
