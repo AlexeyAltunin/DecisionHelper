cf6361d63a70f4de2c6aa90cee9a44beb39c4fd6
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2018-01-21 20:13:28 +0100

    added logic for optionsCriteriaViewComtroller

---
+++ DecisionHelper/ViewControllers/OptionsComparisonTableViewController.swift
@@ -0,0 +1,86 @@
+//
+//  OptionsComparisonTableViewController.swift
+//  DecisionHelper
+//
+//  Created by ALEXEY ALTUNIN on 21/01/2018.
+//  Copyright © 2018 ALEXEY ALTUNIN. All rights reserved.
+//
+
+import UIKit
+
+class OptionsComparisonTableViewController: UITableViewController {
+
+    var options: [Option]?
+    var criteria: [Criteria]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(options)
+        print(criteria)
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+}
--- DecisionHelper/ViewControllers/OptionsCriteriaTableViewController.swift
+++ DecisionHelper/ViewControllers/OptionsCriteriaTableViewController.swift
@@ -10,18 +10,82 @@ import UIKit
 
 class OptionsCriteriaTableViewController: UITableViewController {
 
+    
+    @IBOutlet weak var option1TextField: UITextField!
+    @IBOutlet weak var option2TextField: UITextField!
+    @IBOutlet weak var option3TextField: UITextField!
+    @IBOutlet weak var option4TextField: UITextField!
+    @IBOutlet weak var option5TextField: UITextField!
+    
+    @IBOutlet weak var criteria1TextField: UITextField!
+    @IBOutlet weak var criteria1SegmentControl: UISegmentedControl!
+    @IBOutlet weak var criteria2TextField: UITextField!
+    @IBOutlet weak var criteria2SegmentControl: UISegmentedControl!
+    @IBOutlet weak var criteria3TextField: UITextField!
+    @IBOutlet weak var criteria3SegmentControl: UISegmentedControl!
+    @IBOutlet weak var criteria4TextField: UITextField!
+    @IBOutlet weak var criteria4SegmentControl: UISegmentedControl!
+    @IBOutlet weak var criteria5TextField: UITextField!
+    @IBOutlet weak var criteria5SegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var options = [Option]()
+    var criteria = [Criteria]()
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let optionTitles = [
+            option1TextField.text,
+            option2TextField.text,
+            option3TextField.text,
+            option4TextField.text,
+            option5TextField.text,
+            ]
+        
+        for optionTitle in optionTitles {
+            if optionTitle != "" {
+                options.append(Option(title: optionTitle!))
+            }
+        }
+        
+        
+        let criteriaTitles: [Int: [String: Int]] = [
+            1: [criteria1TextField.text!: criteria1SegmentControl.selectedSegmentIndex],
+            2: [criteria2TextField.text!: criteria2SegmentControl.selectedSegmentIndex],
+            3: [criteria3TextField.text!: criteria3SegmentControl.selectedSegmentIndex],
+            4: [criteria4TextField.text!: criteria4SegmentControl.selectedSegmentIndex],
+            5: [criteria5TextField.text!: criteria5SegmentControl.selectedSegmentIndex]
+        ]
+        var importance: Importance?
+        
+        for critery in criteriaTitles {
+            for criteriaTitle in critery.value {
+                if criteriaTitle.key != "" {
+                    
+                    switch criteriaTitle.value {
+                    case 0:
+                        importance = Importance.level1
+                    case 1:
+                        importance = Importance.level2
+                    case 2:
+                        importance = Importance.level3
+                    case 3:
+                        importance = Importance.level4
+                    default: break
+                    }
+                    
+                    criteria.append(Criteria(title: criteriaTitle.key, importance: importance!))
+                }
+            }
+        }
+        
+        guard let optionsComparisonViewController = segue.destination as? OptionsComparisonTableViewController else {return}
+        
+        if  segue.identifier == "toOprionsComparison" {
+            optionsComparisonViewController.options = options
+            optionsComparisonViewController.criteria = criteria
+        }
     }
 }
