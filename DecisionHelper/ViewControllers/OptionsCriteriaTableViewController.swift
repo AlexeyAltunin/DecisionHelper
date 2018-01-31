0ef9554a70f8ebdd3c0aacbc1b742ba877ea5ad9
Alexey Altunin <alexey.altunin@frontiercargroup.com>
2018-02-01 00:07:01 +0100

    change demoDescriptionAlert font

--- DecisionHelper/ViewControllers/OptionsCriteriaTableViewController.swift
+++ DecisionHelper/ViewControllers/OptionsCriteriaTableViewController.swift
@@ -200,12 +200,24 @@ class OptionsCriteriaTableViewController: UITableViewController, UITextFieldDele
             self.exampleAlert()
         }
         
+        let myString  = "Пример использования"
+        var myMutableString = NSMutableAttributedString()
+        myMutableString = NSMutableAttributedString(string: myString as String, attributes: [NSAttributedStringKey.font:UIFont(name: "HelveticaNeue-Bold", size: 18.0)!])
+        myMutableString.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.black, range: NSRange(location:0,length:myString.characters.count))
+        
         let message = "В бытность мою в С—м уезде мне часто приходилось бывать на Дубовских огородах у огородника Саввы Стукача, или попросту Савки. Эти огороды были моим излюбленным местом для так называемой «генеральной» рыбной ловли, когда, уходя из дому, не знаешь дня и часа, в которые вернешься, забираешь с собой все до одной рыболовные снасти и запасаешься провизией. Собственно говоря, меня не так занимала рыбная ловля, как безмятежное шатанье, еда не вовремя, беседа с Савкой и продолжительные очные ставки с тихими летними ночами. Савка был парень лет 25, рослый, красивый, здоровый, как кремень. Слыл он за человека рассудительного и толкового, был грамотен, водку пил редко, но как работник этот молодой и сильный человек не стоил и гроша медного. Рядом с силой в его крепких, как веревка, мышцах разливалась тяжелая, непобедимая лень. Жил он, как и все на деревне, в собственной избе, пользовался наделом, но не пахал, не сеял и никаким ремеслом не занимался. Старуха мать его побиралась под окнами, и сам он жил, как птица небесная: утром не знал, что будет есть в полдень. Не то, чтобы у него не хватало воли, энергии или жалости к матери, а просто так, не чувствовалось охоты к труду и не сознавалась польза его... От всей фигуры так и веяло безмятежностью, врожденной, почти артистической страстью к житью зря, спустя рукава. Когда же молодое, здоровое тело Савки физиологически потягивало к мышечной работе, то парень ненадолго весь отдавался какой-нибудь свободной, но вздорной профессии вроде точения ни к чему не нужных колышков или беганья с бабами наперегонку. Самым любимым его положением была сосредоточенная неподвижность. Он был в состоянии простаивать целые часы на одном месте, не шевелясь и глядя в одну точку. Двигался же по вдохновению и то только, когда представлялся случай сделать какое-нибудь быстрое, порывистое движение: ухватить бегущую собаку за хвост, сорвать с бабы платок, перескочить широкую яму. Само собою разумеется, что при такой скупости на движения Савка был гол как сокол и жил хуже всякого бобыля. С течением времени должна была накопиться недоимка, и он, здоровый и молодой, был послан миром на стариковское место, в сторожа и пугало общественных огородов. Как ни смеялись над ним по поводу его преждевременной старости, но он и в ус не дул. Это место, тихое, удобное для неподвижного созерцания, было как раз по его натуре."
+        
+        var messageMutableString = NSMutableAttributedString()
+        messageMutableString = NSMutableAttributedString(string: message as String, attributes: [NSAttributedStringKey.font:UIFont(name: "Georgia", size: 18.0)!])
+        messageMutableString.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.darkText, range: NSRange(location:0,length:message.characters.count))
         
         // Create and configure the alert controller.
         let alert = UIAlertController(title: "Пример использования",
                                       message: message,
                                       preferredStyle: .actionSheet)
+        alert.setValue(myMutableString, forKey: "attributedTitle")
+        alert.setValue(messageMutableString, forKey: "attributedMessage")
+        
         alert.addAction(okAction)
         
         self.present(alert, animated: false) {
